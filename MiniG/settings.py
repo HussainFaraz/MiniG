b59a1c0eda365c3bdccb85d716df7f34daaecbaf
Faraz <farazhusain5148@gmail.com>
2021-06-19 12:04:14 +0530

    updating settings

--- MiniG/settings.py
+++ MiniG/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 import os.path
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'Static'),)
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
