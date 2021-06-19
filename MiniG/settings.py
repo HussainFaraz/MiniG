640bb5b9a8f2f94ae082d33f9f17dca5fe1f5894
Faraz <farazhusain5148@gmail.com>
2021-06-19 14:50:34 +0530

    updating settings

--- MiniG/settings.py
+++ MiniG/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 import os.path
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 #STATIC_ROOT = BASE_DIR / 'static'
 
