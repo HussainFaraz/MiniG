b3c9b6ffce6d0753b5c15f311e9a2a05ac98b443
Faraz <farazhusain5148@gmail.com>
2021-06-19 12:46:30 +0530

    updating settings

--- MiniG/settings.py
+++ MiniG/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -121,9 +121,11 @@ USE_TZ = True
 
 import os.path
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-#STATICFILES_DIRS=(os.path.join(BASE_DIR,'Static'),)
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'Static'),)
+
+
+#STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Extra places for collectstatic to find static files.
